a913f89b33437e891db42ff9fb836770a0f90e32
Juan Ospina <juanospinasuarez@gmail.com>
2015-02-26 17:41:25 -0500

    Primera version

---
+++ app/controllers/articles_controller2.rb
@@ -0,0 +1,30 @@
+class ArticlesController < ApplicationController
+	def new		#1
+	end
+	def create	#2
+		render plain: params[:article].inspect
+	#end	
+	#def create	#3
+	#	@article=Article.new(params[:article])
+	#	@article.save
+	#	redirect_to @article
+	#	@article = Article.new(params.require(:article).permit(:title, :text))
+	#end
+	#def create
+  		@article = Article.new(article_params)
+ 
+ 		@article.save
+  		redirect_to @article
+	end
+ 
+	private
+  	def article_params
+    	params.require(:article).permit(:title, :text)
+  	end
+  	def show
+	    @article = Article.find(params[:id])
+	end
+	 
+	def new
+	end
+end
